4fc387573d627fd4257458279eac9d6ec97d36f3
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-08-05 16:02:37 +0200

    feat: declare vis-tasks

--- libs/vis-tasks/src/lib/types.ts
+++ libs/vis-tasks/src/lib/types.ts
@@ -37,16 +37,31 @@ export type VegaLiteMark =
 
 export type MarkPreference = {
   mark: VegaLiteMark;
+
+  /**
+   * Weight denoting the importance of this preference.
+   */
   weight: number;
 };
 
 /**
- * Associates a ``VisTask`` with an array of ``MarkPreference``s.
- *
  * Facilitates expressing which marks are preferred (and to what extent)
  * for a given visualization task.
  * (E.g. for the "Comparison" task, we prefer "line", "point" or "bar")
  */
-export type VisTaskWithPreferences = VisTask & {
-  preferences: MarkPreference[];
+export type VisTaskPreferences = {
+  /**
+   * Explicitly declared marks which are **optimal** (favored) for this task.
+   */
+  favors: MarkPreference[];
+
+  /**
+   * Explicitly declared marks which are **sub-optimal** (disfavored) for this task.
+   */
+  disfavors: MarkPreference[];
 };
+
+/**
+ * Associates a ``VisTask`` with its ``VisTaskPreferences``s.
+ */
+export type VisTaskWithPreferences = VisTask & VisTaskPreferences;
