4614dbd3d2695c06d713418d8f8d7ba92489f69f
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-08-10 22:41:48 +0200

    feat: implement `DataColumnSelector`

--- apps/dashboard/modules/encoding-preferences/data-column-selector/views/DataColumnSelector.tsx
+++ apps/dashboard/modules/encoding-preferences/data-column-selector/views/DataColumnSelector.tsx
@@ -1,3 +1,7 @@
+import tw from 'twin.macro';
+
+import { snakeCaseToHumanCase } from '@dashboard/modules/encoding-preferences/data-column-selector/utils/utils';
+import DataColumnTypeIcon from '@dashboard/modules/encoding-preferences/data-column-selector/views/DataColumnTypeIcon';
 import { useRecInput } from '@dashboard/modules/rec-input/provider/RecInputContext';
 import { DataColumn as DataColumnType } from '@dashboard/modules/rec-input/types/types';
 
@@ -6,7 +10,12 @@ function DataColumnSelector() {
     state: { availableDataColumns, selectedDataColumns },
     dispatch,
   } = useRecInput();
-  const handleSelect = (columns: DataColumnType[]) => {};
+  const handleSelect = (columns: DataColumnType[]) => {
+    dispatch({
+      type: 'setSelectedDataColumns',
+      data: columns.map(({ name }) => name),
+    });
+  };
   return (
     <_DataColumnSelector
       availableDataColumns={availableDataColumns}
@@ -25,17 +34,27 @@ type DataColumnSelectorProps = {
 function _DataColumnSelector({
   availableDataColumns,
   selectedDataColumns,
+  onSelect,
 }: DataColumnSelectorProps) {
+  const handleClick = (column: DataColumnType) => {
+    const newSelectedColumns =
+      selectedDataColumns.find((c) => c.name === column.name) !== undefined
+        ? selectedDataColumns.filter((c) => c.name !== column.name)
+        : [...selectedDataColumns, column];
+    onSelect(newSelectedColumns);
+  };
   return (
     <div className="flex flex-col">
       {availableDataColumns.map(({ name, type }) => {
-        const isSelected = true;
+        const isSelected =
+          selectedDataColumns.find((col) => col.name === name) !== undefined;
         return (
           <DataColumn
             key={`data-column-${name}`}
             name={name}
             type={type}
             isSelected={isSelected}
+            onClick={() => handleClick({ name, type })}
           />
         );
       })}
@@ -47,12 +66,26 @@ type DataColumnProps = {
   name: string;
   type: string;
   isSelected: boolean;
+  onClick: () => void;
 };
 
-function DataColumn({ name, type, isSelected }: DataColumnProps) {
+const styles = {
+  container: ({ isSelected }: { isSelected: boolean }) => [
+    tw`my-1 flex space-x-2 items-center border-2 cursor-pointer transition-all duration-[250ms]`,
+    tw`hover:bg-primary-500 hover:text-white`,
+    tw`active:bg-primary-600 active:scale-105`,
+    isSelected && tw`bg-primary-700 text-white`,
+  ],
+};
+
+function DataColumn({ name, type, isSelected, onClick }: DataColumnProps) {
+  const humanCasedName = snakeCaseToHumanCase(name);
   return (
-    <div>
-      {name} - {type} - {JSON.stringify(isSelected)}
+    <div css={styles.container({ isSelected })} onClick={onClick}>
+      <div className="p-2">
+        <DataColumnTypeIcon type={type} />
+      </div>
+      <p>{humanCasedName}</p>
     </div>
   );
 }
