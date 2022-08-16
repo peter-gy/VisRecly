ff694d081edc751fcd3de101afacc24a7d61cdca
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-08-16 12:50:00 +0200

    feat: consider `minHeight` in `MobileGate`

--- apps/dashboard/modules/layout/views/MobileGate.tsx
+++ apps/dashboard/modules/layout/views/MobileGate.tsx
@@ -5,16 +5,25 @@ import { useTheme } from '@mui/material/styles';
 
 type MobileGateProps = {
   minWidth?: number;
+  minHeight?: number;
   standardChild: ReactNode;
   mobileChild: ReactNode;
 };
 
-function MobileGate({ minWidth, standardChild, mobileChild }: MobileGateProps) {
+function MobileGate({
+  minWidth,
+  minHeight,
+  standardChild,
+  mobileChild,
+}: MobileGateProps) {
   const theme = useTheme();
   const { lg } = theme.breakpoints.values;
   const _minWidth = minWidth ?? lg;
-  const match = useMediaQuery(`(min-width: ${_minWidth}px)`);
-  return <>{match ? standardChild : mobileChild}</>;
+  const widthMatch = useMediaQuery(`(min-width: ${_minWidth}px)`);
+
+  const _minHeight = minHeight ?? lg / 1.5;
+  const heightMatch = useMediaQuery(`(min-height: ${_minHeight}px)`);
+  return <>{widthMatch && heightMatch ? standardChild : mobileChild}</>;
 }
 
 export default MobileGate;
