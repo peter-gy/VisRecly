8215f97798eaedeb3edacd5d930ea0786ad99f46
Péter Ferenc Gyarmati <40776291+peter-gy@users.noreply.github.com>
2022-08-30 16:51:58 +0200

    feat: lighthouse enhancements (#48) (#49)
    * chore: flag `productionBrowserSourceMaps`
    
    address "Missing source maps for large first-party JavaScript"
    
    * chore: use `webpack-bundle-analyzer`
    
    * chore: minify all json files
    
    * fix: do not revert json minification
    
    * chore: add `analyze` nx target
    
    * chore: fix analyze config

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -13,5 +13,14 @@ const nextConfig = {
   optimizeFonts: true,
   swcMinify: false,
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
 };
-module.exports = withNx(nextConfig);
+
+if (process.env.NODE_ENV === 'development' && process.env.ANALYZE === 'true') {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  });
+  module.exports = withBundleAnalyzer(withNx(nextConfig));
+} else {
+  module.exports = withNx(nextConfig);
+}
